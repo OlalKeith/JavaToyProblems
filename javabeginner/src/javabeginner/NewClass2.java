e73fa51d91c33f973affe23f79b2c274257c183c
OlalKeith <jkeneth.jk@gmail.com>
2015-09-16 15:08:11 +0300

    basics

---
+++ javabeginner/src/javabeginner/NewClass2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javabeginner;
+
+/**
+ *
+ * @author olalkeith
+ */
+public class NewClass2 {
+    
+    public static void donut(String [] args){
+        
+        String name = "james";
+        int age = 28;
+        double annualpay =1000.0;
+        Javabeginner.hello();
+         Javabeginner.hello();
+        
+    }
+}
