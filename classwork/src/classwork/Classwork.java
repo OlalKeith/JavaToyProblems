3a818f559250e759310c153050ba5adfc681c775
OlalKeith <jkeneth.jk@gmail.com>
2015-09-18 10:21:54 +0300

    usingscanner

---
+++ classwork/src/classwork/Classwork.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classwork;
+
+/**
+ *
+ * @author olalkeith
+ */
+import java.util.Scanner;
+public class Classwork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner james = new Scanner (System.in);
+        int age, likes;
+       System.out.println("Enter age:");
+    age= james.nextInt();
+    System.out.println("Enter likes:");
+     likes= james.nextInt();
+    
+        // TODO code application logic here
+    }
+    
+}
