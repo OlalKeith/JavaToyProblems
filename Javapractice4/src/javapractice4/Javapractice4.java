0a9d790f855831e58ca09cb5fa928c607f85344e
OlalKeith <jkeneth.jk@gmail.com>
2015-09-18 04:37:44 +0300

    practice

---
+++ Javapractice4/src/javapractice4/Javapractice4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice4;
+
+/**
+ *
+ * @author olalkeith
+ */
+import java.util.Scanner;
+public class Javapractice4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner james =new Scanner (System.in);
+        System.out.println(james.nextLine());hey 
+        // TODO code application logic here
+    }
+    
+}
