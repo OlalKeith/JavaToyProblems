19fcc7f3e55d6132a24ca7e856adbdb1eb8e4533
OlalKeith <jkeneth.jk@gmail.com>
2015-09-22 06:16:03 +0300

    whileloop

---
+++ Javapractice8/src/javapractice8/Javapractice8.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice8;
+
+/**
+ *
+ * @author olalkeith
+ */
+public class Javapractice8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 10){
+            System.out.println(counter);
+            counter++;
+        }
+        // TODO code application logic here
+    }
+    
+}
