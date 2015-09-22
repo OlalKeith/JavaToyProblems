66914106425b006de48086f7b51408fef900f435
OlalKeith <jkeneth.jk@gmail.com>
2015-09-22 08:04:51 +0300

    for loops

---
+++ Javapractice9/src/javapractice9/Javapractice9.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice9;
+
+/**
+ *
+ * @author olalkeith
+ */
+public class Javapractice9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int counter=6;counter<=21;counter+=3){
+            System.out.println(counter);
+        }
+        // TODO code application logic here
+    }
+    
+}
