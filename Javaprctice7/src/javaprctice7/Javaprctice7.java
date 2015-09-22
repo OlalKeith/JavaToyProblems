3a721618a510c9d2714612c35e02673edcb20d83
OlalKeith <jkeneth.jk@gmail.com>
2015-09-22 05:44:49 +0300

    incrementoperators

---
+++ Javaprctice7/src/javaprctice7/Javaprctice7.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprctice7;
+
+/**
+ *
+ * @author olalkeith
+ */
+import java.util.Scanner;
+public class Javaprctice7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner james= new Scanner(System.in);
+        int tuna = 5;
+        int bass = 18;
+      tuna += 8; 
+        System.out.println(tuna);
+        // TODO code application logic here
+    }
+    
+}
