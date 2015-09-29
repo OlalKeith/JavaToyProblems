a0b06f9f38daba16b9e086a9e5b7a05cff17e1a5
OlalKeith <jkeneth.jk@gmail.com>
2015-09-29 11:31:09 +0300

    forloop

--- paren_matcher/paren_matcher.py
+++ paren_matcher/paren_matcher.py
@@ -1,7 +1,17 @@
-# Write a function that return whether or not the input string has balanced parantheses
-# Balanced:
-#   '((()))'
-#   '(()())'
-# Not balanced:
-#   '((()'
-#   '())('
+
+def parent(str):
+    #declaring a couple of variables to count the number of occurencies of each parenthesis
+    opener = 0
+    closer = 0
+    #looping through the str using for loop
+    for i in str:
+        if i == '(':
+            opener += 1
+        elif i == ')':
+            closer += 1
+    return opener == closer
+
+print parent(raw_input())
+    
+    
+
