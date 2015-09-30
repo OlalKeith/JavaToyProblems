5c9f98e53aa4f7ac9b402a0f0626bbd925265ad3
OlalKeith <jkeneth.jk@gmail.com>
2015-09-30 09:37:12 +0300

    solution

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -19,5 +19,15 @@
 # gigasecond(1988, 5, 15) # ["2020-01-22", "Wednesday", "1764 days left"]
 # gigasecond(2015, 2, 17) # ["2046-10-26", "Friday", "11538 days left"]
 
-import datetime
-today = datetime.date.today()
+from datetime import date, timedelta
+
+WEEKDAYS = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+def gigasecond(year, month, day):
+    giga_birthsecond = date(year, month, day) + timedelta(seconds=1000000000)
+    birthday = WEEKDAYS[giga_birthsecond.weekday()]
+    days_left = (giga_birthsecond - date.today()).days
+
+    print(giga_birthsecond, birthday, days_left, "days left")
+    
+gigasecond(1989, 4, 29)
