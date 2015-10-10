f9b1420d0fbb29fdbcf86969d5736e091c973f50
OlalKeith <jkeneth.jk@gmail.com>
2015-10-10 18:42:16 +0300

    incremental operators

---
+++ operators/src/operators/Operators.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operators;
+
+/**
+ *
+ * @author olalkeith
+ */
+import java.util.Scanner;
+public class Operators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        Scanner james = new Scanner(System.in);
+        
+        int tuna = 5;
+        int bass = 18;
+        ++tuna;
+        
+        System .out.println(tuna);
+    }
+    
+}
