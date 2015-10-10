60a36a97160ec97aba1532fe94854f6bc59923e2
OlalKeith <jkeneth.jk@gmail.com>
2015-10-10 19:41:40 +0300

    logical operators

---
+++ logical operators/src/logical/operators/LogicalOperators.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logical.operators;
+
+/**
+ *
+ * @author olalkeith
+ */
+public class LogicalOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int boy, girl;
+        boy = 18;
+        girl = 50;
+        
+        if (boy > 10 && girl < 49){
+        System.out.println("you can enter");
+    }else{
+          System.out.println("you cannot enter");  
+            }
+    }
+    
+}
