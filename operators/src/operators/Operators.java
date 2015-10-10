1101548f83648b47935e40135f246d83ccd25c40
OlalKeith <jkeneth.jk@gmail.com>
2015-10-10 18:50:14 +0300

    incremental operators

--- operators/src/operators/Operators.java
+++ operators/src/operators/Operators.java
@@ -21,7 +21,9 @@ public class Operators {
         
         int tuna = 5;
         int bass = 18;
-        ++tuna;
+        
+        tuna += 5;
+      
         
         System .out.println(tuna);
     }
